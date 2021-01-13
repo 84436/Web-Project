fe8054eda2915f546253e14d24e778eec5497c26
Where is Neo? <alec.bui.vhp@gmail.com>
2021-01-13 20:41:52 +0700

    category logics and data backup

--- models/categoryModel.js
+++ models/categoryModel.js
@@ -1,69 +1,91 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
 var categorySchema = new mongoose.Schema({
     major: String,
-    minor: String
-})
+    minor: String,
+});
 
 categorySchema.index({
     major: "text",
-    minor: "text"
-})
+    minor: "text",
+});
 
-var categoryModel = mongoose.model("category", categorySchema, "categories")
+var categoryModel = mongoose.model("category", categorySchema, "categories");
+
+var courseModel = require("./courseModel");
 
 /********************************************************************************/
 
 async function search(query) {
-    let filter = { $text: { $search: query } }
-    let projection = { __v: 0 }
+    let filter = { $text: { $search: query } };
+    let projection = { __v: 0 };
     return await categoryModel.find(filter, projection, (err) => {
-        return null
-    })
+        return null;
+    });
 }
 
 async function getByID(id) {
-    let projection = { __v: 0 }
-    return await categoryModel.findById(id, projection, (err) => {
-        return null
-    }).lean()
+    let projection = { __v: 0 };
+    return await categoryModel
+        .findById(id, projection, (err) => {
+            return null;
+        })
+        .lean();
 }
 
 async function getAll() {
-    let projection = { __v: 0 }
-    return await categoryModel.find({}, projection, (err) => {
-        return null
-    }).lean()
+    let projection = { __v: 0 };
+    return await categoryModel
+        .find({}, projection, (err) => {
+            return null;
+        })
+        .lean();
 }
 
 async function add(major, minor) {
     let newCategory = new categoryModel({
         major: major,
-        minor: minor
-    })
-    newCategory.save((err) => {})
+        minor: minor,
+    });
+    newCategory.save((err) => {});
 }
 
 async function edit(id, newMajor, newMinor) {
     let update = {
         major: newMajor,
-        minor: newMinor
-    }
-    let options = { upsert: false }
-    await categoryModel.findByIdAndUpdate(id, update, options, (err) => {})
+        minor: newMinor,
+    };
+    let options = { upsert: false };
+    await categoryModel.findByIdAndUpdate(id, update, options, (err) => {});
 }
 
 async function remove(id) {
-    await categoryModel.findByIdAndDelete(id, (err) => {})
+    await categoryModel.findByIdAndDelete(id, (err) => {});
+}
+
+async function removeIfEmpty(id) {
+    let r = { _err: null };
+    let courses = await courseModel.get_course_by_category(id);
+
+    if (courses === null) {
+        r._err = "Error in checking empty category.";
+    } else if (courses.length === 0) {
+        await remove(id);
+    } else {
+        r._err = "Cannot remove category. Maybe it contains courses.";
+    }
+
+    return r;
 }
 
 /********************************************************************************/
 
 module.exports = {
-    search  : search,
-    getByID : getByID,
-    getAll  : getAll,
-    add     : add,
-    edit    : edit,
-    remove  : remove
-}
+    search: search,
+    getByID: getByID,
+    getAll: getAll,
+    add: add,
+    edit: edit,
+    remove: remove,
+    removeIfEmpty: removeIfEmpty,
+};
--- models/courseModel.js
+++ models/courseModel.js
@@ -1,4 +1,4 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
 var courseSchema = new mongoose.Schema({
     // course + instructor
@@ -6,15 +6,15 @@ var courseSchema = new mongoose.Schema({
     banner: String, // image path
     categoryID: {
         type: mongoose.Schema.Types.ObjectId,
-        ref: "category"
+        ref: "category",
     },
     desc: {
         short: String,
-        long: String
+        long: String,
     },
     instructorID: {
         type: mongoose.Schema.Types.ObjectId,
-        ref: "account"
+        ref: "account",
     },
 
     // pricing
@@ -23,24 +23,24 @@ var courseSchema = new mongoose.Schema({
 
     // metrics
     averageRate: Number,
-    views: [{
-        type: Date
-    }],
+    viewCount: Number,
     enrollCount: Number,
     feedbackCount: Number,
 
     // content
     lastUpdated: Date,
     isFinished: Boolean,
-    content: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "course_chapter"
-    }]
-})
+    content: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "course_chapter",
+        },
+    ],
+});
 
 courseSchema.index({
-    name: "text"
-})
+    name: "text",
+});
 
 /**
  * Course: has Chapters
@@ -51,81 +51,136 @@ courseSchema.index({
 var courseChapterSchema = new mongoose.Schema({
     courseID: {
         type: mongoose.Schema.Types.ObjectId,
-        ref: "course"
+        ref: "course",
     },
     name: String,
-    lessons: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "course_lesson"
-    }]
-})
+    lessons: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "course_lesson",
+        },
+    ],
+});
 
 var courseLessonSchema = new mongoose.Schema({
     chapterID: {
         type: mongoose.Schema.Types.ObjectId,
-        ref: "course_chapter"
+        ref: "course_chapter",
     },
     name: String,
     video: String, // video path
-    text: String // actual content goes here
-})
+    text: String, // actual content goes here
+});
 
-var courseModel = mongoose.model("course", courseSchema, "courses")
-var courseChapterModel = mongoose.model("course_chapter", courseChapterSchema, "course_chapters")
-var courseLessonModel = mongoose.model("course_lesson", courseLessonSchema, "course_lessons")
+var courseModel = mongoose.model("course", courseSchema, "courses");
+var courseChapterModel = mongoose.model(
+    "course_chapter",
+    courseChapterSchema,
+    "course_chapters"
+);
+var courseLessonModel = mongoose.model(
+    "course_lesson",
+    courseLessonSchema,
+    "course_lessons"
+);
 
 /********************************************************************************/
 // Courses
 
 async function search_course(query) {
-    let filter = { $text: { $search: query } }
-    let projection = { __v: 0 }
-    return await courseModel.find(filter, projection, (err) => { return null })
-                            .lean()
-                            .populate({
-                                path: "instructorID",
-                                select: "name"
-                             })
+    let filter = { $text: { $search: query } };
+    let projection = { __v: 0 };
+    return await courseModel
+        .find(filter, projection, (err) => {
+            return null;
+        })
+        .lean()
+        .populate({
+            path: "instructorID",
+            select: "name",
+        });
 }
 
 // Get everything about this course and also populates ToC
 async function get_course(courseID) {
-    let projection = { __v: 0 }
-    let r =  await courseModel.findById(courseID, projection, (err) => {
-                                  return null
-                               })
-                              .lean()
-                              .populate({
-                                   path: "categoryID",
-                                   select: "major minor"
-                               })
-                              .populate({
-                                   path: "instructorID",
-                                   select: "name"
-                               })
-                              .populate({
-                                  path: "content",
-                                  populate: {
-                                      path: "lessons",
-                                      select: "name"
-                                  }
-                               })
-                              
-    return r
+    let projection = { __v: 0 };
+    let r = await courseModel
+        .findById(courseID, projection, (err) => {
+            return null;
+        })
+        .lean()
+        .populate({
+            path: "categoryID",
+            select: "major minor",
+        })
+        .populate({
+            path: "instructorID",
+            select: "name",
+        })
+        .populate({
+            path: "content",
+            populate: {
+                path: "lessons",
+                select: "name",
+            },
+        });
+
+    return r;
+}
+
+async function getByCategory(categoryID) {
+    let filter = {
+        categoryID: categoryID,
+    };
+
+    let r = await courseModel
+        .find(filter, (err) => {
+            return null;
+        })
+        .lean()
+        .populate({
+            path: "instructorID",
+            select: "name",
+        });
+
+    return r;
+}
+
+async function countEnrolledByCat() {
+    let r = await courseModel.aggregate([
+        {
+            $group: {
+                _id: "$categoryID",
+                totalEnroll: { $sum: "$enrollCount" },
+            },
+        },
+        {
+            $sort: {
+                totalEnroll: -1,
+            },
+        },
+    ]);
+
+    return r;
 }
 
 async function add_course(courseInfo) {
-    let newCourse = new courseModel(courseInfo)
-    newCourse.save((err) => {})
+    let newCourse = new courseModel(courseInfo);
+    newCourse.save((err) => {});
 }
 
 async function update_course(courseID, courseInfo) {
-    let options = { upsert: false }
-    await courseModel.findByIdAndUpdate(courseID, courseInfo, options, (err) => {})
+    let options = { upsert: false };
+    await courseModel.findByIdAndUpdate(
+        courseID,
+        courseInfo,
+        options,
+        (err) => {}
+    );
 }
 
 async function remove_course(courseID) {
-    await courseModel.findByIdAndDelete(courseID, (err) => {})
+    await courseModel.findByIdAndDelete(courseID, (err) => {});
 }
 
 /********************************************************************************/
@@ -136,35 +191,42 @@ async function remove_course(courseID) {
 function add_chapter(courseID, chapterName) {
     let newChapter = new courseChapterModel({
         courseID: courseID,
-        name: chapterName
-    })
-    let newChapterID
+        name: chapterName,
+    });
+    let newChapterID;
     newChapter.save((err, doc) => {
-        newChapterID = doc.id
-    })
-    return newChapterID
+        newChapterID = doc.id;
+    });
+    return newChapterID;
 }
 
 async function update_chapter(chapterID, chapterName) {
     let update = {
-        name: chapterName
-    }
-    let options = { upsert: false }
-    await courseChapterModel.findByIdAndUpdate(chapterID, update, options, (err) => {})
+        name: chapterName,
+    };
+    let options = { upsert: false };
+    await courseChapterModel.findByIdAndUpdate(
+        chapterID,
+        update,
+        options,
+        (err) => {}
+    );
 }
 
 async function remove_chapter(chapterID) {
-    await courseChapterModel.findByIdAndDelete(chapterID, (err) => {})
+    await courseChapterModel.findByIdAndDelete(chapterID, (err) => {});
 }
 
 /********************************************************************************/
 // Lessons
 
 async function get_lesson(lessonID) {
-    let projection = { __v: 0 }
-    return await courseLessonModel.findById(lessonID, projection, (err) => {
-        return null
-    }).lean()
+    let projection = { __v: 0 };
+    return await courseLessonModel
+        .findById(lessonID, projection, (err) => {
+            return null;
+        })
+        .lean();
 }
 
 async function add_lesson(chapterID, lessonInfo) {
@@ -172,38 +234,45 @@ async function add_lesson(chapterID, lessonInfo) {
         chapterID: chapterID,
         name: lessonInfo.name,
         text: lessonInfo.text,
-        video: lessonInfo.video
-    })
-    newLesson.save((err) => {})
+        video: lessonInfo.video,
+    });
+    newLesson.save((err) => {});
 }
 
 async function update_lesson(lessonID, lessonInfo) {
     let update = {
         name: lessonInfo.name,
         text: lessonInfo.text,
-        video: lessonInfo.video
-    }
-    let options = { upsert: false }
-    await courseLessonModel.findByIdAndUpdate(lessonID, update, options, (err) => {})
+        video: lessonInfo.video,
+    };
+    let options = { upsert: false };
+    await courseLessonModel.findByIdAndUpdate(
+        lessonID,
+        update,
+        options,
+        (err) => {}
+    );
 }
 
 async function remove_lesson(lessonID) {
-    await courseLessonModel.findByIdAndDelete(lessonID, (err) => {})
+    await courseLessonModel.findByIdAndDelete(lessonID, (err) => {});
 }
 
 /********************************************************************************/
 
 module.exports = {
-    search_course  : search_course,
-    get_course     : get_course,
-    add_course     : add_course,
-    update_course  : update_course,
-    remove_course  : remove_course,
-    add_chapter    : add_chapter,
-    update_chapter : update_chapter,
-    remove_chapter : remove_chapter,
-    get_lesson     : get_lesson,
-    add_lesson     : add_lesson,
-    update_lesson  : update_lesson,
-    remove_lesson  : remove_lesson
-}
+    search_course: search_course,
+    get_course: get_course,
+    add_course: add_course,
+    update_course: update_course,
+    remove_course: remove_course,
+    add_chapter: add_chapter,
+    update_chapter: update_chapter,
+    remove_chapter: remove_chapter,
+    get_lesson: get_lesson,
+    add_lesson: add_lesson,
+    update_lesson: update_lesson,
+    remove_lesson: remove_lesson,
+    getByCategory: getByCategory,
+    countEnrolledByCat: countEnrolledByCat,
+};
