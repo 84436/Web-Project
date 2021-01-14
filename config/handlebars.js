ea1bc08d0f1765d833a952e7b3b4755f8f6186b3
84436 <thien.bui.84436@gmail.com>
2021-01-14 23:05:41 +0700

    [*] hbs: edit config, split navbar from main

--- config/handlebars.js
+++ config/handlebars.js
@@ -2,12 +2,16 @@ const express_handlebars = require('express-handlebars')
 const path = require('path')
 
 // https://github.com/ericf/express-handlebars
+// https://stackoverflow.com/a/40898191
 function configure(app) {
     const ExpressHandlebarsOptions = {
-        defaultLayout: 'main.hbs'
+        extname: 'hbs',
+        defaultLayout: 'main',
+        layoutsDir: path.join(__dirname, '../views/layouts'),
+        partialsDir: path.join(__dirname, '../views/partials')
     }
     app.engine('hbs', express_handlebars(ExpressHandlebarsOptions))
-    app.set('views', path.join(__dirname, '../views'))
+    // app.set('views', path.join(__dirname, '../views'))
     app.set('view engine', 'hbs')
 }
 
