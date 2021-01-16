eb872ba75794644f105c0c2d33bfe01d9792ab76
84436 <thien.bui.84436@gmail.com>
2021-01-16 22:44:59 +0700

    [*] separate home for each roles (mostly navbar changes only)

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -4,19 +4,33 @@ const router = express.Router()
 const categoryModel = require("../models/categoryModel")
 const courseModel = require("../models/courseModel");
 
-router.get('/', async (i, o) => {    
+router.get('/', async (i, o, next) => {    
     o.locals.catList = await categoryModel.getAll();
     o.locals.hotCourses = await courseModel.topEnrollCourse()
     o.locals.mostViewCourses = await courseModel.topViewCourses()
     o.locals.newestCourses = await courseModel.newestCourses()
     o.locals.topCategories = await courseModel.topEnrollCategories()
-    let a = i.session.User
-    
-    if (a) {
-        o.locals.User = a 
+
+    o.locals.User = i.session.User
+
+    if (!o.locals.User) {
+        o.render('home/homeGuest')
     }
-    
-    o.render('home');
+    else {
+        switch (o.locals.User.type) {
+            case "student":
+                o.render('home/homeStudent')
+                break;
+            case "instructor":
+                o.render('home/homeInstructor')
+                break;
+            case "admin":
+                o.render('home/homeAdmin')
+                break;
+            default:
+                next(new Error())
+        }
+    }
 })
 
 module.exports = router
