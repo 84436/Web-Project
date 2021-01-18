1dd60ce1ec6cbd85cd4cff0fa23b7a217e1e6437
Where is Neo? <alec.bui.vhp@gmail.com>
2021-01-18 19:05:22 +0700

    Update hbsHelpers,view lecturer's courses

--- helpers/hbshelper.js
+++ helpers/hbshelper.js
@@ -1,5 +1,13 @@
 module.exports = {
     eq(str1, str2) {
         return str1.toString() === str2.toString();
+    },
+    dateFormat(date){
+        const dFormat = new Date(date);
+        return dFormat.toLocaleDateString(("en-ZA"));
+    },
+    calSaleOff(price, discount){
+        const saleOffPrice = price - price*discount;
+        return saleOffPrice.toFixed(2);
     }
 };
